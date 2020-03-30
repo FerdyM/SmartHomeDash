181741d739412c8c8f8b1cecdcf0f3e46f9f31d8
Ferdy Macleod <ferdymacleod21@gmail.com>
2020-03-30 16:54:44 +1000

    more frontend work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Card from './components/Main'
+import Main from './components/Main'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Card />
+      <Main />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,68 +1,97 @@
-import React, {Component} from 'react';
-import FormGroup from '@material-ui/core/FormGroup';
+import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Slider from '@material-ui/core/Slider';
 import Switch from '@material-ui/core/Switch';
 import Link from '@material-ui/core/Link';
+// import { SketchPicker } from 'react-color'
 import './Main.css';
 
 const fakeAPIcall = () => {
+    fetch('http://localhost:8080/lights/lavalamp1', requestOptions)
     console.log('this will trigger a lamp by calling the express backend');
 }
 
-const Card = () => {
+const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ switch: 'off' })
+};
+
+
+const Main = () => {
     
 
     return (
         <div className="main">
-            <div className="lights">
-                <h2>Lights</h2>
-                <FormControlLabel 
-                    className="saltLampLightSwitch"
-                    control= {
-                        <Switch 
-                            color="primary" 
-                            onChange={fakeAPIcall()}
-                        />
-                    }    
-                    label="Salt Lamp"
-                />
-                <FormControlLabel 
-                    className="rgbSaltLampLightSwitch"
-                    control= {
-                        <Switch 
-                            color="primary" 
-                            onChange={fakeAPIcall()}
-                        />
-                    }    
-                    label="RGB Salt Lamp"
-                />
-            </div>   
-            <div className="navigation">
-                <h2>Smart Home Hub</h2>
-                <Link 
-                    href="10.0.0.230:5000"
-                    className="dash-link"
-                >
-                    OctoPrint
-                </Link>  
-                <Link 
-                    href="10.0.0.230:1880/ui"
-                    className="dash-link"
-                >
-                    Node-red Dashboard
-                </Link> 
-                <Link 
-                    href="10.0.0.230:5000/admin"
-                    className="dash-link"
-                >
-                    Pi-hole Dashboard
-                </Link> 
-            </div>
+            <LightSwitches />
+            <NavigationPane /> 
         </div>
     );
 }
+const LightSwitches = () => {
+    return (
+        <div className="lights">
+            <h2>Lights</h2>
+            <FormControlLabel 
+                className="saltLampLightSwitch"
+                control= {
+                    <Switch 
+                        color="primary" 
+                        onChange={fakeAPIcall()}
+                    />
+                }    
+                label="Salt Lamp"
+            />
+            <FormControlLabel 
+                className="rgbSaltLampLightSwitch"
+                control= {
+                    <Switch 
+                        color="primary" 
+                        onChange={fakeAPIcall()}
+                    />
+                }    
+                label="RGB Salt Lamp"
+            />
+            <FormControlLabel 
+                className="lavaLamp"
+                control= {
+                    <Switch 
+                        color="primary" 
+                        onChange={fakeAPIcall()}
+                    />
+                }    
+                label="Lava Lamp"
+            />
+            {/* <SketchPicker /> */}
+        </div> 
+    )
+}
 
-export default Card;
+
+const NavigationPane = () => {
+    return (
+        <div className="navigation">
+            <h2>Smart Home Hub</h2>
+            <Link 
+                href="127.0.0.1:5000"
+                target="_blank"
+                className="dash-link"
+            >
+                OctoPrint
+            </Link>  
+            <Link 
+                href="10.0.0.230:1880/ui"
+                className="dash-link"
+            >
+                Node-red Dashboard
+            </Link> 
+            <Link 
+                href="10.0.0.230:5000/admin"
+                className="dash-link"
+            >
+                Pi-hole Dashboard
+            </Link> 
+        </div>
+    )
+}
+
+export default Main;
